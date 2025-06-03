a9dc4c323d7b6d449ed6c651a59df9aa79f61e7e
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-03 09:33:39 +0600

    6/3/2025

--- src/components/Expression.jsx
+++ src/components/Expression.jsx
@@ -13,6 +13,7 @@ function Expression() {
       <h1>i am in {obj.Department}</h1><br />
       <h1>My address is {obj.Address}</h1><br />
       <p>lets have some random number each time the page is reloaded .The random number will be {Math.random()*100}</p>
+      <h1>{`My name is ${obj.name}. I am in ${obj.Department}`}</h1>
     </div>
   )
 }
