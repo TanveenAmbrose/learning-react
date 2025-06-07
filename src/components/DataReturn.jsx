444fc63123ff518825c8192f34290a9fcb13d585
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-07 11:30:12 +0600

    fetching done:

--- src/components/DataReturn.jsx
+++ src/components/DataReturn.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
 
-function DataReturn() {
+
+const DataReturn=(props)=> {
   return (
     <>
-    <Navbar/>
+    <div className="container text-center p-4 m-2 border-4 border-indigo-500">
+        <p>{props.id}</p>
+        <p>{props.name}</p>
+        <p>{props.email}</p>
+        <p>{props.phone}</p>
+    </div>
     </>
   )
 }
--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
+import DataReturn from './DataReturn';
 
-function FetchData() {
+const FetchData =()=> {
+    const [saveData, setData]= useState(null);
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res)=> res.json())
+        .then((data)=> setData(data))
+    },[])
   return (
     <>
     <Navbar/>
+    <div className="container">
+        {saveData &&
+            saveData.map((data)=>{
+                return <DataReturn name={data.name} email={data.email} id={data.id}
+                phone={data.phone}/>
+            })
+        }
+    </div>
     </>
   )
 }
