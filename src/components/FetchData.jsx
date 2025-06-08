53bcee07a40b46b0f478e632b3870f3310641c68
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-08 12:08:08 +0600

    Image adding done

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react'
 import DataReturn from './DataReturn'
 import Navbar from './Navbar';
 
+
 function FetchData() {
     const [saveData, setData] = useState(null);
+    const [saveImg,setImg] = useState(null)
     useEffect(()=>{
         setTimeout(()=>{
             fetch("https://jsonplaceholder.typicode.com/users")
@@ -11,16 +13,31 @@ function FetchData() {
             .then((data)=>setData(data))
         },1000)
     },[])
+    useEffect(()=>{
+        setTimeout(()=>{
+            fetch("https://dog.ceo/api/breeds/image/random")
+            .then((res)=>res.json())
+            .then((dataImg)=>setImg(dataImg))
+        },2000)
+    },[])
   return (
+    <>
     <div>
         <Navbar/>
     {
-        saveData &&
+        saveData && 
         saveData.map((data)=>{
            return <DataReturn name={data.name} email={data.email} id={data.id} street={data.address.street}/>
         })
     }
+    {
+          saveImg &&
+          <img src={saveImg.message} alt="" />
+          
+        }
     </div>
+    </>
+    
   )
 }
 
